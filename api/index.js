593c96f518883a09ae267f12d4d4fd8d1fb9c74a
NOONECANSEE <anshusingla405@gmail.com>
2024-03-15 18:21:06 +0530

    SIGNUP API

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
-const app = express();
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
@@ -11,9 +13,10 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-app.get("/", (req, res) => {
-  res.send("hllo");
-});
+const app = express();
+app.use(express.json());
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
