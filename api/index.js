fe10accbbc429e8e90555ad9cf54c0d3d5ed3eff
NOONECANSEE <anshusingla405@gmail.com>
2024-03-15 18:31:00 +0530

    MIDDLEWARE

--- api/index.js
+++ api/index.js
@@ -15,8 +15,21 @@ mongoose
   });
 const app = express();
 app.use(express.json());
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
+});
+
+// ROUTES
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
