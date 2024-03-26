03509168d08ffbd4115bb03f488450065d317160
NOONECANSEE <anshusingla405@gmail.com>
2024-03-26 18:36:38 +0530

    DELETE FUNCTIONALITY

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -35,3 +35,16 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "You Can only Delete your account"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User Deleted");
+  } catch (error) {
+    next(error);
+  }
+};
