7ceb32218e65da1b693cdf0e3be14f105ee9dac8
NOONECANSEE <anshusingla405@gmail.com>
2024-03-29 10:06:32 +0530

    GET USER LISTING API

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -4,7 +4,7 @@ export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
 
   if (!token) {
-    return next(errorHandler(401, "Unauthorized"));
+    return next(errorHandler(401, "Please login again"));
   }
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return next(errorHandler(403, "Forbidden"));
