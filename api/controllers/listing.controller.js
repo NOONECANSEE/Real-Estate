578196599195cb48770da056f0b57c3cdb2df610
NOONECANSEE <anshusingla405@gmail.com>
2024-04-03 11:47:47 +0530

    UPDATED SEARCH FUNCTIONALITY

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -65,8 +65,8 @@ export const getListing = async (req, res, next) => {
 export const getListings = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 6;
-    const startIdx = parseInt(req.query.startIndex) || 0;
-    const offer = req.query.offer;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
     if (offer === undefined || offer === "false") {
       offer = { $in: [false, true] };
     }
@@ -101,7 +101,7 @@ export const getListings = async (req, res, next) => {
     })
       .sort({ [sort]: order })
       .limit(limit)
-      .skip(startIdx);
+      .skip(startIndex);
 
     return res.status(200).json(listings);
   } catch (error) {
